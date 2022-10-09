121b8b81ea23e0b0cea46a9623343cd6ac329c9c
Jacob Grum <jacg@itu.dk>
2022-10-09 12:36:31 +0200

    a

--- server/server.go
+++ server/server.go
@@ -92,14 +92,14 @@ func (s *Server) JoinChat(clientMessage *pb.ClientRequest, stream pb.ChatService
 	if s.messageChannels[clientMessage.ChatMessage.Userid] == nil {
 		s.messageChannels[clientMessage.ChatMessage.Userid] = make(chan *pb.ChatMessage)
 	}
-	enteredMessage := fmt.Sprint("User joined chat: ", clientMessage.ChatMessage.Userid, " Total users: ", len(s.messageChannels))
+	/* enteredMessage := fmt.Sprint("User joined chat: ", clientMessage.ChatMessage.Userid, " Total users: ", len(s.messageChannels))
 	chatmsg := &pb.ChatMessage{
 		Message:     enteredMessage,
 		Userid:      clientMessage.ChatMessage.Userid,
 		LamportTime: clientMessage.ChatMessage.LamportTime,
 	}
 	log.Println(enteredMessage)
-	stream.Send(chatmsg)
+	stream.Send(chatmsg) */
 	////Keep them in chatroom until they leave.
 	for {
 		select {
