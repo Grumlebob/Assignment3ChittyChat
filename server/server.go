625f5776358c00a209b2c00567fdad157f735d11
Jacob Grum <jacg@itu.dk>
2022-10-09 11:19:33 +0200

    s

--- server/server.go
+++ server/server.go
@@ -53,7 +53,7 @@ func (s *Server) GetClientId(ctx context.Context, clientMessage *pb.ClientReques
 		}
 		idgenerator = rand.Intn(math.MaxInt32)
 	}
-	fmt.Println("generated new user with ID:", idgenerator)
+	//fmt.Println("generated new user with ID:", idgenerator)
 
 	return &pb.ServerResponse{
 		ChatMessage: &pb.ChatMessage{
