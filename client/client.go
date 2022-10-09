f8c20aa03acf092d01767f4356e6580d3b29343a
Jacob Grum <jacg@itu.dk>
2022-10-09 12:26:39 +0200

    a

--- client/client.go
+++ client/client.go
@@ -95,14 +95,14 @@ func joinChat(client pb.ChatServiceClient, context context.Context) {
 			if err != nil {
 				log.Fatalf("Failed to receive message from channel joining. \nErr: %v", err)
 			}
-			if userId != message.Userid {
-				if message.LamportTime > lamportTime {
-					lamportTime = message.LamportTime + 1
-				} else {
-					lamportTime++
-				}
-				fmt.Println("Lamport time now:", lamportTime)
+			//if userId != message.Userid {
+			if message.LamportTime > lamportTime {
+				lamportTime = message.LamportTime + 1
+			} else {
+				lamportTime++
 			}
+			fmt.Println("Lamport time now:", lamportTime)
+			//}
 			log.Println("User:", message.Userid, "- Lamport time:", message.LamportTime, "- Msg:", message.Message)
 		}
 	}()
